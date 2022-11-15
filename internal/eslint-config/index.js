c078fc1f1ac1e8068ba32e1f8c970c640fa481b5
webB1an <webB1anyaoyao@gmail.com>
2022-11-15 09:29:28 +0800

    chore(other): change name

--- internal/eslint-config/index.js
+++ internal/eslint-config/index.js
@@ -249,7 +249,7 @@ module.exports = defineConfig({
             position: 'before',
           },
           {
-            pattern: '@element-plus/**',
+            pattern: '@w-ui/**',
             group: 'internal',
           },
         ],
